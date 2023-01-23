58b6458ce536d6ceeea5c9dfa286ea49ca702e38
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-22 19:15:48 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
+import threading
+
+threading.Thread(target=lambda a: app.run())
 from cloudlink import cloudlink
 if __name__ == "__main__":
     cl = cloudlink() # Instanciate the parent cloudlink object
@@ -11,6 +14,3 @@ if __name__ == "__main__":
         ip = "127.0.0.1", # Defaults to 127.0.0.1, use 0.0.0.0 to host on all interfaces
         port = 5000 # Defaults to port 3000
     )
-    
-    # server.run() is blocking,
-    # no further code will execute unless a CTRL+C, SIGTERM, or SIGKILL is detected.
