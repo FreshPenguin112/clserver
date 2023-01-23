b4cb8132eaff324a26fcc2c2b83416c0a381c380
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-22 19:09:11 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,14 @@
 from cloudlink import cloudlink
-if True:
+if __name__ == "__main__":
     cl = cloudlink() # Instanciate the parent cloudlink object
     server = cl.server(logs=True) # Initialize a new server object
+    
+    # Do configuration here
+    
     server.run(
-        ip = "localhost",
-        port = 8080
+        ip = "localhost", # Defaults to 127.0.0.1, use 0.0.0.0 to host on all interfaces
+        port = 3000 # Defaults to port 3000
     )
+    
+    # server.run() is blocking,
+    # no further code will execute unless a CTRL+C, SIGTERM, or SIGKILL is detected.
