55a261f6041b649fcea69abbc6bff41fe96d2152
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-22 19:07:09 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,8 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.before_request
-def before_request():
-    print('hi')
-    from cloudlink import cloudlink
-    if True:
-        cl = cloudlink() # Instanciate the parent cloudlink object
-        server = cl.server(logs=True) # Initialize a new server object
-        server.run(
-            ip = "0.0.0.0",
-            port = 8080
-        )
-@app.route('/')
-def hello_world(): 
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+from cloudlink import cloudlink
+if True:
+    cl = cloudlink() # Instanciate the parent cloudlink object
+    server = cl.server(logs=True) # Initialize a new server object
+    server.run(
+        ip = "0.0.0.0",
+        port = 8080
+    )
